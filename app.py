41dc7a8df0d5ca42f655bc34177545ba68411ead
BirWri <wrightbirgit@gmail.com>
2025-04-07 12:28:15 +0200

    generate random number for a book id

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 import requests
+import random
 
 def get_random_book():
-    # Step 1: Get total number of books
+    # Get total number of books
     base_url = "https://gutendex.com/books"
     response = requests.get(base_url)
     data = response.json()
     print(data['count'])
+    
+    # Generate random int between 1 and the number of books
+    book_id = random.randint(1, data['count'])
+    print(book_id)
 
 # === Run the script ===
 if __name__ == "__main__":
